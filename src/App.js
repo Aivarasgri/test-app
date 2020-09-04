d9c13fdc5a2e2fbf4e0f6f3f7a0a2314bb1251d9
aivarasgri <agrigonis@harbortouch.com>
2020-09-04 18:54:24 +0300

    DEVMARKET-4444: merge-branche-with-develop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './my-git-playground/footer';
 import NavBar from './my-git-playground/navBar';
 
 function App() {
