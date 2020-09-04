4865f78b06feb91c6dd4d7f38c1c039be57671f3
aivarasgri <agrigonis@harbortouch.com>
2020-09-04 12:01:41 +0300

    DEVMARKET-4444: add-navbar-and-footer

---
+++ src/my-git-playground/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const footer = () => (
+    <h1>I'm Footer</h1>
+)
+
+export default footer;
---
+++ src/my-git-playground/navBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const navBar = () => (
+    <h1>I'm Nav Bar</h1>
+)
+
+export default navBar;
